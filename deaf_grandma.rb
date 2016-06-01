94a9375371ac3ecbb64e028f20a7c3e346a27669
Michael Norelli <mnorelli360@gmail.com>
2016-05-31 22:10:40 -0700

    complete assignment

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,19 +1,25 @@
-count = 0
+# track the number of Grandma's responses
+count = 1
 puts "GRANDMA: Hi there Sonny!  How are you?"
-name = gets
 
-until name == "BYE"
-  if count > 19
+loop do
+  name = gets  # put response into name
+  break if name.chomp == "BYE"   # stop if BYE is entered
+  name = name.chomp.gsub(/\W|\d/, '')   # drop trailing newline, numbers, and punctuation
+  if count > 19            # after twenty times, Grandma's had enough
     shesays = "ZZZZZZ"
-  elsif count % 5 == 0 && count > 5
+  elsif ['JESUS','GOD'].include? name    # no blasphemy!
+    shesays = "You should never take the Lord's name in vain!"
+  elsif ['DAMN','SHIT'].include? name      # no cursing!
+    shesays = "Such language!"
+  elsif count % 5 == 0 && count > 0   # Grandma is easily bored
     shesays = "GRANDMA: It's getting late, dear."
-  elsif name.upcase == name
+  elsif name != '' && name.upcase == name     # she can hear shouting
     shesays = "GRANDMA: No, not since 1928!"
-  else
+  else                            # but nothing else
     shesays = "GRANDMA: What's that, Sonny?"
   end
   puts shesays
   count += 1
-  name = gets
 end
 
